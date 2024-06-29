15db0b83293540095dfe69e8314b6aad8501a7a3
Daggersoath <9066690+Daggersoath@users.noreply.github.com>
2024-06-29 10:34:23 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 wget -O - https://github.com/fastfetch-cli/fastfetch/releases/download/2.17.1/fastfetch-linux-amd64.deb | dpkg -i
 bash <(curl -L https://raw.githubusercontent.com/ChrisTitusTech/linutil/main/start.sh)
+
+cd ~
+mkdir titus
+cd titus
+git clone https://github.com/ChrisTitusTech/mybash.git
+cd mybash
+./setup.sh
