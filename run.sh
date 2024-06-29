ae747e290a2277e9e396980144904711c7d510ed
Daggersoath <9066690+Daggersoath@users.noreply.github.com>
2024-06-29 09:18:34 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+wget -O - https://github.com/fastfetch-cli/fastfetch/releases/download/2.17.1/fastfetch-linux-amd64.deb | dpkg -i
+bash <(curl -L https://raw.githubusercontent.com/ChrisTitusTech/linutil/main/start.sh)
